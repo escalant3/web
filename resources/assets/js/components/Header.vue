43d404e0838761bb088fad963dce602d1e824c5a
Jaime Sares <jaimesares@gmail.com>
2017-01-10 02:21:56 +0100

    Añadidas las imágenes del home y el logo sólo texto

---
+++ resources/assets/js/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="header container">
+        <div class="row">
+            <div class="col-md-12">
+                <img src="/images/logos/logo.png">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component ready.')
+        }
+    }
+</script>
